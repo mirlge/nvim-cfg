d37ba0e1db5ec4554663f685da987d14d841d02d
MineItRiGrEight <tor@barbanet.com>
2023-04-11 17:52:32 +0200

    fix(netrw): Reenabled netrw
    It's only strongly advised to disable netrw when you use nvim-tree

--- lua/mirge/set.lua
+++ lua/mirge/set.lua
@@ -1,6 +1,5 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
---vim.g.netrw_liststyle = 3
+--vim.g.loaded_netrw = 1
+--vim.g.loaded_netrwPlugin = 1
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
