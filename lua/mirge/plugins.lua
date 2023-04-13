51d2471321d74064f3d787326accd076ed7b6b59
MineItRiGrEight <tor@barbanet.com>
2023-04-13 20:16:43 +0200

    feat(noice.nvim): Removed noice.nvim
    The notifications clutter up everything

--- lua/mirge/plugins.lua
+++ lua/mirge/plugins.lua
@@ -187,20 +187,6 @@ local plugins = {
     opts = {},
   },
   {
-    "folke/noice.nvim",
-    opts = {
-      -- add any options here
-    },
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
-    },
-  },
-  {
     "folke/which-key.nvim",
     config = function()
       require "mirge.plugins.which-key"
