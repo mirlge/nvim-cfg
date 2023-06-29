faeb8c5bf099bf80ab5860970f6cc92331c36549
MineItRiGrEight <tor@barbanet.com>
2023-06-29 18:39:08 +0200

    fix(distant.nvim): Fixed distant.nvim by updating it

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -254,15 +254,10 @@ M.plugins = {
   hologram = { 'edluffy/hologram.nvim', opts = { auto_display = true } },
   distant = {
     'chipsenkbeil/distant.nvim',
-    branch = 'v0.2',
-    opts = {
-      -- Applies Chip's personal settings to every machine you connect to
-      --
-      -- 1. Ensures that distant servers terminate with no connections
-      -- 2. Provides navigation bindings for remote directories
-      -- 3. Provides keybinding to jump into a remote file's parent directory
-      --['*'] = require('distant.settings').chip_default()
-    },
+    branch = 'v0.3',
+    config = function()
+      require("core.plugins.distant")
+    end,
   },
   glow = {
     "ellisonleao/glow.nvim",
