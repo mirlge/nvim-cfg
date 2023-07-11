d1989b1cf42d5e02518508a5e9d09fa9d3e6f2a0
MineItRiGrEight <tor@barbanet.com>
2023-07-11 19:18:54 +0200

    fix(multicursors): Fixed error with multicursors.nvim caused by the plugin table not being up-to-date

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -526,7 +526,33 @@ local plugins = {
   multicursors = {
     "smoka7/multicursors.nvim",
     event = "VeryLazy",
-    opts = {},
+    dependencies = {
+      'smoka7/hydra.nvim',
+    },
+    opts = function()
+      local N = require 'multicursors.normal_mode'
+      local I = require 'multicursors.insert_mode'
+      return {
+        normal_keys = {
+          -- to change default lhs of key mapping change the key
+          ['b'] = {
+            -- assigning nil to method exits from multi cursor mode
+            method = N.clear_others,
+            -- description to show in hint window
+            desc = 'Clear others'
+          },
+        },
+        insert_keys = {
+          -- to change default lhs of key mapping change the key
+          ['<CR>'] = {
+            -- assigning nil to method exits from multi cursor mode
+            method = I.Cr_method,
+            -- description to show in hint window
+            desc = 'new line'
+          },
+        },
+      }
+    end,
     keys = {
       {
         '<Leader>m',
