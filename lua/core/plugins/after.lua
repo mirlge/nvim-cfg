60c5420e01950b49456fc2390b4415c637bea555
MineItRiGrEight <tor@barbanet.com>
2023-09-06 13:26:46 +0200

    fix(neorg): Renamed Neorg plugin table from 'norg' to 'neorg'
    Norg is the format, Neorg is the plugin

--- lua/core/plugins/after.lua
+++ lua/core/plugins/after.lua
@@ -3,8 +3,8 @@ local corePlugins = require("core.plugins")
 if corePlugins.null_ls.enabled == nil or corePlugins.null_ls.enabled then
   require("core.plugins.null-ls")
 end
-if corePlugins.norg.enabled == nil or corePlugins.norg.enabled then
-  require("core.plugins.norg")
+if corePlugins.neorg.enabled == nil or corePlugins.neorg.enabled then
+  require("core.plugins.neorg")
 end
 if corePlugins.orgmode.enabled == nil or corePlugins.orgmode.enabled then
   require("core.plugins.orgmode")
