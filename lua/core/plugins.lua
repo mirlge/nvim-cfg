7b4c0798d51d05b4a4782878a9d31570e8e56feb
MineItRiGrEight <tor@barbanet.com>
2023-09-13 09:14:34 +0200

    feat(workflow)!: Add visual.nvim

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -763,6 +763,11 @@ local plugins = {
   rainbow_delimiters = {
     "HiPhish/rainbow-delimiters.nvim",
   },
+  visual = {
+    '00sapo/visual.nvim',
+    event = "VeryLazy", -- this is for making sure our keymaps are applied after the others: we call the previous mapppings, but other plugins/configs usually not!
+    opts = {},
+  },
 }
 
 return plugins
