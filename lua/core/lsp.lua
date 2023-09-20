5bd99d5724afc5cae75e627e8c9d029c7eb4054f
MineItRiGrEight <tor@barbanet.com>
2023-09-20 07:43:51 +0200

    feat(lsp): Add lspsaga.nvim

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,13 +1,3 @@
-vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
-  callback = function(client, bufnr)
-    vim.keymap.set("n", "<Leader>lf", vim.lsp.buf.format, { desc = "Format the current buffer" })
-    vim.keymap.set("n", "<Leader>lca", vim.lsp.buf.code_action, { desc = "Code action" })
-
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP hover" })
-  end,
-})
-
 vim.api.nvim_create_autocmd('BufWritePre', {
   desc = 'Format buffer',
   callback = function()
