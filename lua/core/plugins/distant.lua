9a7a02970a6825dae6c5cd0ccf7cfacd139491c0
MineItRiGrEight <80468882+MineItRiGrEight@users.noreply.github.com>
2023-12-22 17:17:12 +0100

    feat(distant): Enabled distant.nvim, moved `:DistantInstall` to build function

--- lua/core/plugins/distant.lua
+++ lua/core/plugins/distant.lua
@@ -1,10 +1,11 @@
 return {
   'chipsenkbeil/distant.nvim',
   branch = 'v0.3',
+  build = function()
+    vim.cmd.DistantInstall()
+  end,
   config = function(_, opts)
     require("distant"):setup(opts)
-
-    vim.cmd.DistantInstall()
   end,
   opts = {
     -- Applies Chip's personal settings to every machine you connect to
@@ -14,5 +15,4 @@ return {
     -- 3. Provides keybinding to jump into a remote file's parent directory
     --['*'] = require('distant.settings').chip_default()
   },
-  enabled = false, -- distant.nvim can't download a compatible distant build
 }
