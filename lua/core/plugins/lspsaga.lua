f874b447765c2cd1fb5b0d5a63374afbb22319bb
MineItRiGrEight <80468882+MineItRiGrEight@users.noreply.github.com>
2023-12-22 12:15:55 +0100

    feat(lsp): Move lsp keybinds to lsp.lua, add new lsp keybinds, require lsp.lua before lazy

--- lua/core/plugins/lspsaga.lua
+++ lua/core/plugins/lspsaga.lua
@@ -9,6 +9,5 @@ return {
   keys = {
     { "K",           function() vim.cmd.Lspsaga("hover_doc") end,   desc = "Hover" },
     { "<Leader>lca", function() vim.cmd.Lspsaga("code_action") end, desc = "Action" },
-    { "<Leader>lf",  vim.lsp.buf.format,                            desc = "Format the current buffer" },
   },
 }
