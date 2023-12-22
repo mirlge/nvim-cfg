b22cffa48c553b7404c5e51c096abd4d9b305e04
MineItRiGrEight <80468882+MineItRiGrEight@users.noreply.github.com>
2023-12-22 19:57:07 +0100

    fix(neogit): Add cmd to plugin table
    Makes the 'Neorg' command accessible before '<Leader>gs' has been
    pressed in Normal mode

--- lua/core/plugins/neogit.lua
+++ lua/core/plugins/neogit.lua
@@ -1,12 +1,19 @@
 return {
-  'NeogitOrg/neogit',
+  "NeogitOrg/neogit",
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    'sindrets/diffview.nvim',
-    'nvim-telescope/telescope.nvim',
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "nvim-telescope/telescope.nvim",
   },
   opts = {},
+  cmd = "Neogit",
   keys = {
-    { "<Leader>gs", function() require("neogit").open({ kind = "split" }) end, desc = "Open Neogit" },
+    {
+      "<Leader>gs",
+      function()
+        require("neogit").open({ kind = "split" })
+      end,
+      desc = "Open Neogit",
+    },
   },
 }
