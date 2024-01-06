588a46041761a60001d14063580b082b35490d01
MineItRiGrEight <80468882+MineItRiGrEight@users.noreply.github.com>
2024-01-06 21:33:14 +0100

    refactor(keybinds)!: Change `<Leader>b<Tab>` and `<Leader>b<S-Tab>` to `<Leader>bn` and `<Leader>bp`

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,23 +2,25 @@ local M = {
   keymaps = {
     --- system clipboard
     -- yank
-    { modes = { "n", "x" }, key = "<Leader>y", opts = { desc = "y, but to system clipboard" }, action = "\"+y" },
-    { modes = { "n", "x" }, key = "<Leader>Y", opts = { desc = "Y, but to system clipboard" }, action = "\"+Y" },
+    { modes = { "n", "x" }, key = "<Leader>y", opts = { desc = "y, but to system clipboard" }, action = '"+y' },
+    { modes = { "n", "x" }, key = "<Leader>Y", opts = { desc = "Y, but to system clipboard" }, action = '"+Y' },
 
     -- paste
     {
       modes = { "n", "x" },
       key = "<Leader>p",
-      action = "\"+p",
+      action = '"+p',
       opts = {
-        desc = "p, but from system clipboard" }
+        desc = "p, but from system clipboard",
+      },
     },
     {
       modes = { "n", "x" },
       key = "<Leader>P",
-      action = "\"+P",
+      action = '"+P',
       opts = {
-        desc = "P, but from system clipboard" },
+        desc = "P, but from system clipboard",
+      },
     },
     {
       modes = "x",
@@ -27,32 +29,34 @@ local M = {
       opts = { desc = "Replace with yanked text without overwriting it" },
     },
 
-
     -- buffers
     {
       modes = "n",
-      key = "<Leader>b<Tab>",
+      key = "<Leader>bn",
       action = vim.cmd.bnext,
       opts = {
-        desc = "Go to next buffer" },
+        desc = "Go to next buffer",
+      },
     },
     {
       modes = "n",
-      key = "<Leader>b<S-Tab>",
+      key = "<Leader>bp",
       action = vim.cmd.bprev,
       opts = {
-        desc = "Go to previous buffer" },
+        desc = "Go to previous buffer",
+      },
     },
     {
       modes = "n",
       key = "<Leader>bd",
       action = vim.cmd.bdelete,
       opts = {
-        desc = "Delete current buffer" },
+        desc = "Delete current buffer",
+      },
     },
 
     { modes = "n", key = "<Leader><Leader>", action = vim.cmd.so, opts = { desc = "Source the current file" } },
-  }
+  },
 }
 
 function M.setup()
