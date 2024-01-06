60262e87130ddc9a007b12693a325afac749de99
MineItRiGrEight <80468882+MineItRiGrEight@users.noreply.github.com>
2024-01-06 15:05:57 +0100

    feat(inlayhints): Attach lsp-inlayhints.nvim on attach of a language server

--- lua/core/plugins/lsp-inlayhints.lua
+++ lua/core/plugins/lsp-inlayhints.lua
@@ -3,6 +3,19 @@ return {
   dependencies = {
     "neovim/nvim-lspconfig",
   },
-  event = "LspAttach",
   opts = {},
+  config = function(_, opts)
+    require("lsp-inlayhints").setup(opts)
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = "UserLspConfig",
+      callback = function(args)
+        if not (args.data and args.data.client_id) then
+          return
+        end
+
+        require("lsp-inlayhints").on_attach(vim.lsp.get_client_by_id(args.data.client_id), args.buf)
+      end,
+    })
+  end,
 }
