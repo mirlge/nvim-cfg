ab13fccea4b2d0cd4996c9e03b1369068be4bd37
MineItRiGrEight <80468882+MineItRiGrEight@users.noreply.github.com>
2024-01-07 12:11:24 +0100

    fix(neogen): Make it not lazy load

--- lua/core/plugins/neogen.lua
+++ lua/core/plugins/neogen.lua
@@ -2,6 +2,7 @@ return {
   "danymat/neogen",
   dependencies = "nvim-treesitter/nvim-treesitter",
   opts = { snippet_engine = "luasnip" },
+  lazy = false,
   -- Uncomment next line if you want to follow only stable versions
   -- version = "*",
   keys = {
