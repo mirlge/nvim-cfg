038e452383ee6b4c885a17d598ef06ee8f7d5830
MineItRiGrEight <80468882+MineItRiGrEight@users.noreply.github.com>
2024-01-07 12:00:24 +0100

    feat(neogen): Set snippet engine

--- lua/core/plugins/neogen.lua
+++ lua/core/plugins/neogen.lua
@@ -1,7 +1,7 @@
 return {
   "danymat/neogen",
   dependencies = "nvim-treesitter/nvim-treesitter",
-  opts = {},
+  opts = { snippet_engine = "luasnip" },
   -- Uncomment next line if you want to follow only stable versions
   -- version = "*",
 }
