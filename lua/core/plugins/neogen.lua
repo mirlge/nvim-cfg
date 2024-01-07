eb7daada40fc03e94094eb77df37946e10c9d274
MineItRiGrEight <80468882+MineItRiGrEight@users.noreply.github.com>
2024-01-07 12:01:07 +0100

    feat(neogen): Add keybind for generating
    Keybind is `<Leader>fd`

--- lua/core/plugins/neogen.lua
+++ lua/core/plugins/neogen.lua
@@ -4,4 +4,13 @@ return {
   opts = { snippet_engine = "luasnip" },
   -- Uncomment next line if you want to follow only stable versions
   -- version = "*",
+  keys = {
+    {
+      "<Leader>fd",
+      function()
+        require("neogen").generate()
+      end,
+      desc = "Generate documentation",
+    },
+  },
 }
