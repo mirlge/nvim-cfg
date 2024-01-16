4bdea7e3ec6e4698960726e61938dc16835a4a56
MineItRiGrEight <80468882+MineItRiGrEight@users.noreply.github.com>
2024-01-16 07:26:20 +0100

    fix(surround): Make `s` key from flash.nvim not be in Visual mode
    visual-surround.nvim has this as prefix for surrounding

--- lua/core/plugins/flash.lua
+++ lua/core/plugins/flash.lua
@@ -11,7 +11,7 @@ return {
   },
   -- stylua: ignore
   keys = {
-    { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end,              desc = "Flash" },
+    { "s", mode = { "n", "o" },      function() require("flash").jump() end,              desc = "Flash" },
     { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
     { "r", mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
     { "R", mode = { "o" },           function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
