c2f6418871bd467f19ae913fb4bdd931ff676bd1
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2024-04-26 07:28:22 +0200

    refactor(undoing)!: Reset 'swapfile', 'backup' and 'undodir' options to their defaults

--- lua/core/set.lua
+++ lua/core/set.lua
@@ -15,9 +15,6 @@ vim.opt.softtabstop = -1
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.updatetime = 50
