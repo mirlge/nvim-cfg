5626340458d73d9c6a7e2848f4abc2cbfb0fed92
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2024-10-14 18:46:47 +0200

    fix(lazy.nvim): Add rounded border around lazy.nvim window
    Makes it not look like it's a part of the background

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -1,14 +1,19 @@
-  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-  if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable", -- latest stable release
-      lazypath,
-    })
-  end
-  vim.opt.rtp:prepend(lazypath)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-  require("lazy").setup("core.plugins")
+require("lazy").setup({
+  spec = "core.plugins",
+  ui = {
+    border = "rounded",
+  },
+})
