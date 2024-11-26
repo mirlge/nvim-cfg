93194a053f5f75382c679548d767e34af52872a9
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2024-11-26 21:24:38 +0100

    fix(arduino): Add workaround, with instructions for how to install the fixed version, for arduino-language-server

--- lua/core/plugins/mason/mason-lspconfig.lua
+++ lua/core/plugins/mason/mason-lspconfig.lua
@@ -21,6 +21,20 @@ return {
         })
       end,
       ["rust_analyzer"] = function() end,
+
+      -- # Arduino language server workaround
+      --
+      -- Compile arduino-language-server from source, on branch 'main' and with
+      -- `replace go.bug.st/lsp => github.com/speelbarrow/go-lsp
+      -- v0.1.3-0.20241103164431-cf1c00fb5806` code added to `go.mod`
+      ["arduino_language_server"] = function(server_name)
+        local mod_capabilities = vim.tbl_deep_extend("force", capabilities, {
+          workspace = { semanticTokens = vim.NIL }
+        })
+        require("lspconfig")[server_name].setup({
+          capabilities = mod_capabilities,
+        })
+      end,
     })
   end,
   opts = {
