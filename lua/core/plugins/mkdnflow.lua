5d94e71ee5e11e550ab65ebaf8bca9cb3c16aa7f
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2024-11-30 21:19:23 +0100

    refactor(mkdnflow)!: Change format of the link created when pressing `<CR>` in a markdown file

--- lua/core/plugins/mkdnflow.lua
+++ lua/core/plugins/mkdnflow.lua
@@ -6,5 +6,13 @@ return {
       cmp = true,
       yaml = true,
     },
+    links = {
+      transform_explicit = function(text)
+        text = text:gsub(" ", "-")
+        text = text:lower()
+        text = os.date('%Y%m%d%H%M_') .. text
+        return (text)
+      end,
+    }
   },
 }
