3bac5fb61d07aeaab8e9d6b4884c1c5ab1b37932
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2024-12-06 23:32:54 +0100

    refactor(lazydev): Update plugin spec

--- lua/core/plugins/lazydev.lua
+++ lua/core/plugins/lazydev.lua
@@ -6,12 +6,11 @@ return {
       library = {
         -- See the configuration section for more details
         -- Load luvit types when the `vim.uv` word is found
-        { path = "luvit-meta/library", words = { "vim%.uv" } },
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
       },
     },
   },
-  { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-  {                                        -- optional completion source for require statements and module annotations
+  { -- optional completion source for require statements and module annotations
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       opts.sources = opts.sources or {}
