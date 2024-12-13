00265a877630c3fbc3f5d531b342e0fbc180020a
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2024-12-13 21:00:17 +0100

    fix: Disable rose-pine transparency if Neovide is the UI

--- lua/core/plugins/rose-pine.lua
+++ lua/core/plugins/rose-pine.lua
@@ -1,25 +1,33 @@
 return {
   'rose-pine/neovim',
   name = 'rose-pine',
-  opts = {
-    styles = { transparency = true },
-    highlight_groups = {
-      TelescopeBorder = { fg = "overlay", bg = "overlay" },
-      TelescopeNormal = { fg = "subtle", bg = "overlay" },
-      TelescopeSelection = { fg = "text", bg = "highlight_med" },
-      TelescopeSelectionCaret = { fg = "love", bg = "highlight_med" },
-      TelescopeMultiSelection = { fg = "text", bg = "highlight_high" },
+  opts = function()
+    local opts = {
+      styles = { transparency = true },
+      highlight_groups = {
+        TelescopeBorder = { fg = "overlay", bg = "overlay" },
+        TelescopeNormal = { fg = "subtle", bg = "overlay" },
+        TelescopeSelection = { fg = "text", bg = "highlight_med" },
+        TelescopeSelectionCaret = { fg = "love", bg = "highlight_med" },
+        TelescopeMultiSelection = { fg = "text", bg = "highlight_high" },
 
-      TelescopeTitle = { fg = "base", bg = "love" },
-      TelescopePromptTitle = { fg = "base", bg = "pine" },
-      TelescopePreviewTitle = { fg = "base", bg = "iris" },
+        TelescopeTitle = { fg = "base", bg = "love" },
+        TelescopePromptTitle = { fg = "base", bg = "pine" },
+        TelescopePreviewTitle = { fg = "base", bg = "iris" },
 
-      TelescopePromptNormal = { fg = "text", bg = "surface" },
-      TelescopePromptBorder = { fg = "surface", bg = "surface" },
+        TelescopePromptNormal = { fg = "text", bg = "surface" },
+        TelescopePromptBorder = { fg = "surface", bg = "surface" },
 
-      VisualNonText = { fg = "overlay", bg = "highlight_med" },
-    },
-  },
+        VisualNonText = { fg = "overlay", bg = "highlight_med" },
+      },
+    }
+
+    if vim.g.neovide then
+      opts.styles.transparency = nil
+    end
+
+    return opts
+  end,
   config = function(_, opts)
     require("rose-pine").setup(opts)
     vim.cmd.colorscheme("rose-pine")
