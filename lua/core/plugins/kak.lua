4120a691a62e20436bcd958dad95600d0a7df1c9
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2025-01-16 19:42:05 +0100

    feat(kak): Add WhichKey dependency
    Makes the integration with it work

--- lua/core/plugins/kak.lua
+++ lua/core/plugins/kak.lua
@@ -1,6 +1,7 @@
 return {
   "mirge/kak.nvim",
   url = "https://codeberg.org/mirge/kak.nvim.git",
+  dependencies = "folke/which-key.nvim",
   event = "VeryLazy", -- load after other plugins, to avoid conflicts in keybinds
   opts = {
     experimental = {
