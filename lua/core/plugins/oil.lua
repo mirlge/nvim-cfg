6766ee4499ad842625090e1d56fe21fa67cf7d07
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2025-05-23 14:56:59 +0200

    refactor(oil)!: Remove `<Leader>fE` keybind
    `:Oil` exists

--- lua/core/plugins/oil.lua
+++ lua/core/plugins/oil.lua
@@ -6,13 +6,6 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
     {
-      "<Leader>fE",
-      function()
-        require("oil").open(vim.fn.input("Path: "))
-      end,
-      desc = "Open file explorer in...",
-    },
-    {
       "<Leader>fe",
       function()
         require("oil").open()
