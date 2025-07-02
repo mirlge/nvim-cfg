b04666b802a2c1b61dda2136421fada39d098f0c
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2025-07-02 13:57:33 +0200

    fix(markview): Update plugin spec

--- lua/core/plugins/markview.lua
+++ lua/core/plugins/markview.lua
@@ -3,14 +3,7 @@ return {
   lazy = false, -- Recommended
   -- ft = "markdown" -- If you decide to lazy-load anyway
 
-  dependencies = {
-    -- You will not need this if you installed the
-    -- parsers manually
-    -- Or if the parsers are in your $RUNTIMEPATH
-    "nvim-treesitter/nvim-treesitter",
-
-    "nvim-tree/nvim-web-devicons",
-  },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
 
   opts = {
     preview = {
