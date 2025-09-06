bd4a173415d3c5af102f8b989337bba120e30521
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2025-09-06 13:04:38 +0200

    feat(tree-sitter): Automatically start tree-sitter if the parser for the language is available

--- lua/core/plugins/treesitter.lua
+++ lua/core/plugins/treesitter.lua
@@ -4,6 +4,16 @@ return {
     lazy = false,
     branch = 'main',
     build = ':TSUpdate',
+    config = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          local lang = vim.treesitter.language.get_lang(args.match)
+          if vim.treesitter.language.add(lang) then
+            vim.treesitter.start()
+          end
+        end,
+      })
+    end
   },
   { import = "core.plugins.treesitter" },
 }
